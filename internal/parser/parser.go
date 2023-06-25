e433d6928b14d7b593c0351a9a929f17cdc2e544
vscode <yogehaoren@gmail.com>
2023-06-25 17:24:40 +0800

    2.4

---
+++ internal/parser/parser.go
@@ -0,0 +1,111 @@
+package parser
+
+import (
+	"monkey/internal/ast"
+	"monkey/internal/lexer"
+	"monkey/internal/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+	errors []string
+}
+
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l,
+		errors: []string{},
+	}
+	// 读取两个token，设置curToken和peekToken
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool{
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool{
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := "expected next token to be %s, got %s instread"
+	p.errors = append(p.errors, msg)
+}
+
+// 期望下一个token是t类型
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		// 读取下一个token
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+
+func (p *Parser) ParseProgram() *ast.Program {
+
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for p.curToken.Type != token.EOF {
+		// 解析语句
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		// 读取下一个token
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		// 解析let语句
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// 解析let后面的标识符
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// 标识符
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// 解析等号
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
