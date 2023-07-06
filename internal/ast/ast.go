ca1ae2ddaca8c230d6ac340c4cf0832c46946dfa
abadfox <yogehaoren@gmail.com>
2023-07-06 14:52:10 +0800

    前缀表达式

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -4,6 +4,7 @@ import "monkey/internal/token"
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -27,7 +28,16 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+func (p *Program) String() string {
 
+	var out string
+
+	for _, s := range p.Statements {
+		out += s.String()
+	}
+
+	return out
 }
 
 type LetStatement struct {
@@ -37,9 +47,23 @@ type LetStatement struct {
 }
 
 func (ls *LetStatement) statementNode() {}
-
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+func (ls *LetStatement) String() string {
+
+	var out string
+
+	out += ls.TokenLiteral() + " "
+	out += ls.Name.String()
+	out += " = "
+
+	if ls.Value != nil {
+		out += ls.Value.String()
+	}
+	out += ";"
+
+	return out
 }
 
 type Identifier struct {
@@ -48,14 +72,15 @@ type Identifier struct {
 }
 
 func (i *Identifier) expressionNode() {}
-
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
-
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 type ReturnStatement struct {
-	Token token.Token // token.RETURN
+	Token       token.Token // token.RETURN
 	ReturnValue Expression
 }
 
@@ -63,4 +88,68 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+func (rs *ReturnStatement) String() string {
 
+	var out string
+
+	out += rs.TokenLiteral() + " "
+
+	if rs.ReturnValue != nil {
+		out += rs.ReturnValue.String()
+	}
+	out += ";"
+
+	return out
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // 表达式的第一个token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+func (es *ExpressionStatement) String() string {
+
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
+type PrefixExpression struct {
+	Token    token.Token // 前缀token，比如!
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+
+	var out string
+
+	out += "("
+	out += pe.Operator
+	out += pe.Right.String()
+	out += ")"
+
+	return out
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,9 +1,36 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/internal/ast"
 	"monkey/internal/lexer"
 	"monkey/internal/token"
+	"strconv"
+)
+
+type (
+	// 前缀解析函数
+	prefixParseFn func() ast.Expression
+	// 中缀解析函数
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+const (
+	_ int = iota
+	// 低优先级
+	LOWEST
+	// ==
+	EQUALS
+	// 比较
+	LESSGREATER
+	// +
+	SUM
+	// *
+	PRODUCT
+	// 前缀表达式
+	PREFIX
+	// 括号、函数调用
+	CALL
 )
 
 type Parser struct {
@@ -11,16 +38,34 @@ type Parser struct {
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l,
 		errors: []string{},
+		prefixParseFns: make(map[token.TokenType]prefixParseFn),
+		infixParseFns:  make(map[token.TokenType]infixParseFn),
 	}
 	// 读取两个token，设置curToken和peekToken
 	p.nextToken()
 	p.nextToken()
+	// 注册前缀解析函数
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	return p
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) nextToken() {
@@ -64,9 +109,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	for p.curToken.Type != token.EOF {
 		// 解析语句
 		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
-		}
+		program.Statements = append(program.Statements, stmt)
 		// 读取下一个token
 		p.nextToken()
 	}
@@ -82,7 +125,7 @@ func (p *Parser) parseStatement() ast.Statement {
 		// 解析return语句
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement();
 	}
 }
 
@@ -121,3 +164,67 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	return stmt
 }
+
+// 解析表达式语句
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+	// 解析表达式
+	stmt.Expression = p.parseExpression(LOWEST)
+	// 解析分号
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+// 解析表达式
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	// 前缀解析函数
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	// 解析前缀表达式
+	leftExp := prefix()
+	return leftExp
+}
+
+// 解析标识符
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+// 解析整型字面量
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	literal := &ast.IntegerLiteral{Token: p.curToken}
+	// 解析整型字面量
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := "could not parse %q as integer"
+		p.errors = append(p.errors, fmt.Sprintf(msg, p.curToken.Literal))
+		return nil
+	}
+	// 整型字面量值
+	literal.Value = value
+	return literal
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := "no prefix parse function for %s found"
+	p.errors = append(p.errors, fmt.Sprintf(msg, t))
+}
+
+// 解析前缀表达式
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	// 读取下一个token
+	p.nextToken()
+	// 解析右侧表达式
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
+
