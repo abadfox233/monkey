a767cbc739a2d6d2180095112650656439bdb51a
abadfox <yogehaoren@gmail.com>
2023-10-13 18:48:24 +0800

    7.3

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -5,6 +5,7 @@ import (
 	"monkey/internal/ast"
 	"monkey/internal/code"
 	"monkey/internal/object"
+	"sort"
 )
 
 type EmittedInstruction struct {
@@ -67,6 +68,24 @@ func (c *Compiler) Compile(node ast.Node) error {
 			}
 		}
 		c.emit(code.OpArray, len(node.Elements))
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for key := range node.Pairs {
+			keys = append(keys, key)
+		}
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+		for _, key := range keys {
+			if err := c.Compile(key); err != nil {
+				return err
+			}
+
+			if err := c.Compile(node.Pairs[key]); err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpHash, len(node.Pairs)*2)
 	case *ast.IfExpression:
 		if err := c.Compile(node.Condition); err != nil {
 			return err
