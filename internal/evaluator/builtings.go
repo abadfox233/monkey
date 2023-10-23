dfea350ffff6adc6dc034642970f294bf9d7a807
abadfox <yogehaoren@gmail.com>
2023-10-23 19:53:55 +0800

    9.1_使修改变得简单

--- internal/evaluator/builtings.go
+++ internal/evaluator/builtings.go
@@ -7,101 +7,17 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len":       {Fn: lenObject},
+	"len":       object.GetBuiltinByName("len"),
 	"timestamp": {Fn: timestamp},
-	"first":     {Fn: first},
-	"last":      {Fn: last},
-	"rest":      {Fn: rest},
-	"push":      {Fn: push},
-	"puts":      {Fn: puts},
+	"first":     object.GetBuiltinByName("first"),
+	"last":      object.GetBuiltinByName("last"),
+	"rest":      object.GetBuiltinByName("rest"),
+	"push":      object.GetBuiltinByName("push"),
+	"puts":      object.GetBuiltinByName("puts"),
 	"int":       {Fn: toInt},
 	"float":     {Fn: toFloat},
 	"str":       {Fn: toStr},
 	"exit":      {Fn: exit},
-}
-
-func lenObject(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1", len(args))
-	}
-
-	switch arg := args[0].(type) {
-	case *object.String:
-		return &object.Integer{Value: int64(len(arg.Value))}
-	case *object.Array:
-		return &object.Integer{Value: int64(len(arg.Elements))}
-	default:
-		return newError("argument to `len` not supported, got %s", args[0].Type())
-	}
-}
-
-func first(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1", len(args))
-	}
-
-	switch arg := args[0].(type) {
-	case *object.Array:
-		if len(arg.Elements) > 0 {
-			return arg.Elements[0]
-		}
-		return NULL
-	default:
-		return newError("argument to `first` not supported, got %s", args[0].Type())
-	}
-}
-
-func last(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1", len(args))
-	}
-
-	switch arg := args[0].(type) {
-	case *object.Array:
-		if len(arg.Elements) > 0 {
-			return arg.Elements[len(arg.Elements)-1]
-		}
-		return NULL
-	default:
-		return newError("argument to `last` not supported, got %s", args[0].Type())
-	}
-}
-
-func rest(args ...object.Object) object.Object {
-
-	if len(args) != 1 {
-		return newError("wrong number of arguments. got=%d, want=1", len(args))
-	}
-
-	switch arg := args[0].(type) {
-	case *object.Array:
-		length := len(arg.Elements)
-		if length > 0 {
-			newElements := make([]object.Object, length-1)
-			copy(newElements, arg.Elements[1:length])
-			return &object.Array{Elements: newElements}
-		}
-		return NULL
-	default:
-		return newError("argument to `rest` not supported, got %s", args[0].Type())
-	}
-
-}
-
-func push(args ...object.Object) object.Object{
-	if len(args) != 2 {
-		return newError("wrong number of arguments. got=%d, want=2", len(args))
-	}
-	if args[0].Type() != object.ARRAY_OBJ {
-		return newError("argument to `push` must be ARRAY, got %s", args[0].Type())
-	}
-	arr := args[0].(*object.Array)
-	length := len(arr.Elements)
-
-	newElements := make([]object.Object, length+1)
-	copy(newElements, arr.Elements)
-	newElements[length] = args[1]
-	return &object.Array{Elements: newElements}
 }
 
 func timestamp(args ...object.Object) object.Object {
@@ -109,13 +25,6 @@ func timestamp(args ...object.Object) object.Object {
 		return newError("wrong number of arguments. got=%d, want=0", len(args))
 	}
 	return &object.Integer{Value: time.Now().Unix()}
-}
-
-func puts(args ...object.Object) object.Object {
-	for _, arg := range args {
-		println(arg.Inspect())
-	}
-	return NULL
 }
 
 func toInt(args ...object.Object) object.Object {
