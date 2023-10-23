b0e93efd83d71313dc7433f2b294d430750cd286
abadfox <yogehaoren@gmail.com>
2023-10-23 10:52:08 +0800

    8.3.1_编译带参数的函数调用

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -115,7 +115,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err := c.Compile(node.Function); err != nil {
 			return err
 		}
-		c.emit(code.OpCall)
+
+		for _, arg := range node.Arguments {
+			if err := c.Compile(arg); err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpCall, len(node.Arguments))
 	case *ast.FunctionLiteral:
 		c.enterScope()
 
